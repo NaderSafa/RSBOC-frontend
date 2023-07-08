3812954ff7a5250e331030a4d0bb43dd6cbfb27a
nader-safa <125364885+nader-safa@users.noreply.github.com>
2023-07-08 03:29:38 +0300

    installed some packages

---
+++ src/server.js
@@ -0,0 +1,22 @@
+// const axios = require('axios').default
+import axios from 'axios'
+
+// COMMENT NEXT 2 LINES TO SWITCH TO LOCAL BACKEND
+const IP = 'rsboc-backend.vercel.app/api'
+const API_URL = `https://${IP}`
+
+// UNCOMMENT NEXT 2 LINES TO SWITCH TO LOCAL BACKEND
+// const IP = 'localhost:8000'
+// const API_URL = `http://${IP}`
+
+export { API_URL }
+
+// Return a custom axios instance
+const server = axios.create({
+  baseURL: `${API_URL}`,
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+  },
+})
+
+export default server
